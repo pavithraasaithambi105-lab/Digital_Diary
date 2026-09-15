8763a05099f9f78c0c79363c8391d04f4f5ca511
PAVITHRA A <pavithraasaithambi105@gmail.com>
2026-09-16 03:45:45 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -13,8 +13,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://digital-diary-9war.vercel.app"
     ],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
