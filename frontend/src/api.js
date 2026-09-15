dcd74490b6f3d2fe5a282b1de244a779d9360b7a
PAVITHRA A <pavithraasaithambi105@gmail.com>
2026-09-16 03:43:06 +0530

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "digital-diary-oj3b.vercel.app",
+    baseURL: "https://digital-diary-oj3b.vercel.app",
 });
 
 export default API;
